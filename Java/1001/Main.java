9de1f04767ddb577ce0ae86e77d46a4b057e5cba
AhsanInkiad <ahsaninkiadakib380082@gmail.com>
2024-05-23 22:48:19 +0600

    Added problem 1000, 1001 (JAVA).

---
+++ Java/1001/Main.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+      int A,B,X;
+      Scanner input=new Scanner(System.in);
+      A=input.nextInt();
+      Scanner input2=new Scanner(System.in);
+      B=input.nextInt();
+      
+      X=A+B;
+        System.out.print("X = "+X);
+        System.out.print("\n");
+        
+    }
+ 
+}
